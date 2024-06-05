bcba0c15d89af25bf296e2e76c9868ddbb6e680a
dheerajcl <cldheeraj541@gmail.com>
2024-06-05 09:51:46 +0530

    Including Response Body

--- app/server.go
+++ app/server.go
@@ -1,41 +1,38 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"net/http"
 	"os"
+	"strings"
 )
 
-func Handler(conn net.Conn) {
-	defer conn.Close()
-	reader, err := http.ReadRequest(bufio.NewReader(conn))
-	if err != nil {
-		fmt.Println("Error reading request. ", err.Error())
-		return
-	}
-	fmt.Printf("Request: %s %s\n", reader.Method, reader.URL.Path)
-
-	if reader.URL.Path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		return
-	}
-	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-}
-
 func main() {
+	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	fmt.Println("Listening on port 4221")
-	defer l.Close()
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	Handler(conn)
+	
+	buffer:= make([]byte, 1024)
+
+	conn.Read(buffer)
+
+	splitHeader:=strings.Split(string(buffer), "\r\n")
+	splitRequestLine:=strings.Split(splitHeader[0], " ")
+
+	if splitRequestLine[1] == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.Split(splitRequestLine[1], "/")[1] == "echo" {
+		requestBody:=strings.Split(splitRequestLine[1], "/")[2]
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+fmt.Sprint(len(requestBody))+"\r\n\r\n"+requestBody))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
