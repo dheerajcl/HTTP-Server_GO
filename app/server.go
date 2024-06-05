190cd1454264ddb490cfa6c87588235762bb73d0
dheerajcl <cldheeraj541@gmail.com>
2024-06-05 10:38:25 +0530

    Gzip Compression

--- app/server.go
+++ app/server.go
@@ -1,131 +1,94 @@
 
 package main
+
 import (
 	"bytes"
+	"compress/gzip"
+	"flag"
 	"fmt"
 	"net"
 	"os"
-	"regexp"
+	"slices"
 	"strings"
 )
-const CRLF = "\r\n"
-type StatusLine struct {
-	method  string
-	path    string
-	version string
-}
-type Headers = map[string]string
-type Body = string
-type Request struct {
-	status  StatusLine
-	headers Headers
-	body    Body
-}
-func parseRequest(buffer []byte) Request {
-	buffer = bytes.Trim(buffer, "\x00")
-	stringBuffer := string(buffer)
-	return Request{
-		status:  makeStatusLine(stringBuffer),
-		headers: makeHeaders(stringBuffer),
-		body:    makeBody(stringBuffer),
-	}
-}
-func makeStatusLine(buffer string) StatusLine {
-	statusLineIndex := strings.Index(buffer, CRLF)
-	statusLine := buffer[:statusLineIndex]
-	stringStatusLine := strings.Fields(statusLine)
-	return StatusLine{
-		method:  stringStatusLine[0],
-		path:    stringStatusLine[1],
-		version: stringStatusLine[2],
-	}
-}
-func makeHeaders(buffer string) Headers {
-	headers := make(map[string]string)
-	lines := strings.Split(buffer, CRLF)
-	for _, line := range lines {
-		if line == "" {
-			break
-		}
-		parts := strings.SplitN(line, ":", 2)
-		if len(parts) == 2 {
-			headers[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
-		}
-	}
-	return headers
-}
-func makeBody(buffer string) Body {
-	bodyIndex := strings.LastIndex(buffer, CRLF)
-	body := buffer[bodyIndex:]
-	body = body[2:]
-	return body
-}
-func handleConnection(connection net.Conn) {
-	buffer := make([]byte, 1024)
-	connection.Read(buffer)
-	request := parseRequest(buffer)
-	homePattern := "/"
-	echoPattern := regexp.MustCompile(`^/echo/[a-zA-Z0-9]+$`)
-	userAgentPattern := "/user-agent"
-	filenamePattern := regexp.MustCompile(`^/files/[a-zA-Z0-9_]+$`)
-	var response string
-	if echoPattern.MatchString(request.status.path) {
-		pathComponents := strings.Split(request.status.path, "/")
-		path := pathComponents[2]
-		value, ok := request.headers["Accept-Encoding"]
-		if ok {
-			if value == "gzip" {
-				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(path), path)
-			} else if strings.Contains(value, "gzip") {
-				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(path), path)
-			} else {
-				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(path), path)
-			}
-		} else {
-			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(path), path)
-		}
-	} else if userAgentPattern == request.status.path {
-		userAgentValue := request.headers["User-Agent"]
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgentValue), userAgentValue)
-	} else if homePattern == request.status.path {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if filenamePattern.MatchString(request.status.path) {
-		pathComponents := strings.Split(request.status.path, "/")
-		filename := pathComponents[2]
-		directory := os.Args[2]
-		if request.status.method == "GET" {
-			data, err := os.ReadFile(directory + filename)
-			if err != nil {
-				fmt.Println("cannot find")
-				response = "HTTP/1.1 404 Not Found\r\n\r\n"
-			} else {
-				fmt.Println("fined")
-				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)
-			}
-		} else {
-			os.WriteFile(directory+filename, []byte(request.body), os.ModeTemporary)
-			response = "HTTP/1.1 201 Created\r\n\r\n"
-		}
-	} else {
-		response = "HTTP/1.1 404 Not Found\r\n\r\n"
-	}
-	byteResponse := []byte(response)
-	connection.Write(byteResponse)
-	connection.Close()
-}
 func main() {
-	fmt.Println("Server was started")
+	fmt.Println("Logs from your program will appear here!")
+	directoryPtr := flag.String("directory", "", "a string")
+	flag.Parse()
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 	for {
-		connection, err := l.Accept()
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(connection)
+		go func() {
+			buff := make([]byte, 2048)
+			_, err = conn.Read(buff)
+			if err != nil {
+				fmt.Println("Error reading request: ", err.Error())
+				os.Exit(1)
+			}
+			request := string(buff)
+			requestMethod := strings.Split(request, " ")[0]
+			requestTarget := strings.Split(request, " ")[1]
+			headers := strings.Split(request, "\r\n")[1:]
+			var response string
+			if requestTarget == "/" {
+				response = "HTTP/1.1 200 OK\r\n\r\n"
+			} else if strings.HasPrefix(requestTarget, "/echo") {
+				echoString := strings.Split(requestTarget, "/echo/")[1]
+				acceptEncodingIndex := slices.IndexFunc(headers, func(h string) bool { return strings.Contains(h, "Accept-Encoding: ") })
+				if acceptEncodingIndex == -1 {
+					response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoString), echoString)
+				} else {
+					acceptEncoding := strings.TrimPrefix(headers[acceptEncodingIndex], "Accept-Encoding: ")
+					if strings.Contains(acceptEncoding, "gzip") {
+						var b bytes.Buffer
+						enc := gzip.NewWriter(&b)
+						enc.Write([]byte(echoString))
+						enc.Close()
+						response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(b.String()), b.String())
+					} else {
+						response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoString), echoString)
+					}
+				}
+			} else if strings.HasPrefix(requestTarget, "/user-agent") {
+				userAgentIndex := slices.IndexFunc(headers, func(h string) bool { return strings.Contains(h, "User-Agent: ") })
+				userAgent := strings.TrimPrefix(headers[userAgentIndex], "User-Agent: ")
+				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+			} else if strings.HasPrefix(requestTarget, "/files") {
+				fileName := strings.TrimPrefix(requestTarget, "/files/")
+				if requestMethod == "GET" {
+					contents, err := os.ReadFile(*directoryPtr + "/" + fileName)
+					if err != nil {
+						response = "HTTP/1.1 404 Not Found\r\n\r\n"
+					} else {
+						response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(contents), contents)
+					}
+				} else if requestMethod == "POST" {
+					body := headers[len(headers)-1]
+					bodyBytes := bytes.Trim([]byte(body), "\x00")
+					fmt.Print(body)
+					err := os.WriteFile(*directoryPtr+"/"+fileName, bodyBytes, 0644)
+					if err != nil {
+						response = ""
+					} else {
+						response = "HTTP/1.1 201 Created\r\n\r\n"
+					}
+				} else {
+					response = ""
+				}
+			} else {
+				response = "HTTP/1.1 404 Not Found\r\n\r\n"
+			}
+			conn.Write([]byte(response))
+			conn.Close()
+		}()
 	}
 }
