628aa84c6f3137dfa835823c67d897509ee1bc4e
dheerajcl <cldheeraj541@gmail.com>
2024-06-05 10:30:31 +0530

    Content Encoding Header

--- app/server.go
+++ app/server.go
@@ -1,62 +1,221 @@
 package main
 import (
+	"bytes"
+	"flag"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"os"
-	"path"
+	"path/filepath"
 	"strings"
 )
+const (
+	CRLF = "\r\n"
+)
+type HTTPRequest struct {
+	Method      string
+	Target      string
+	HTTPVersion string
+	Headers     map[string]string
+	Body        []byte
+}
+func (r HTTPRequest) String() string {
+	return fmt.Sprintf("%s %s %s %v", r.Method, r.Target, r.HTTPVersion, r.Body)
+}
+func byteReader(channel chan []byte, connection net.Conn) error {
+	buffer := make([]byte, 0)
+	for {
+		tmp := make([]byte, 1024)
+		n, err := connection.Read(tmp)
+		if err != nil && err != io.EOF {
+			close(channel)
+			return err
+		}
+		buffer = append(buffer, tmp[:n]...)
+		splitBuffer := bytes.Split(buffer, []byte(CRLF))
+		for _, line := range splitBuffer[:len(splitBuffer)-1] {
+			channel <- line
+		}
+		buffer = splitBuffer[len(splitBuffer)-1]
+		if n <= len(tmp) || err == io.EOF {
+			break
+		}
+	}
+	channel <- buffer
+	close(channel)
+	return nil
+}
+func parseRequest(connection net.Conn) (HTTPRequest, error) {
+	lines := make(chan []byte)
+	go byteReader(lines, connection)
+	requestLineValues := bytes.Split(<-lines, []byte(" "))
+	if len(requestLineValues) != 3 {
+		return HTTPRequest{}, fmt.Errorf("invalid request line")
+	}
+	method := string(requestLineValues[0])
+	target := string(requestLineValues[1])
+	httpVersion := string(requestLineValues[2])
+	headers := make(map[string]string)
+	for {
+		headerLine := <-lines
+		if len(headerLine) == 0 {
+			break
+		}
+		headerLineValues := bytes.Split(headerLine, []byte(": "))
+		headers[string(headerLineValues[0])] = string(headerLineValues[1])
+	}
+	body := <-lines
+	return HTTPRequest{
+		Method:      method,
+		Target:      target,
+		HTTPVersion: httpVersion,
+		Headers:     headers,
+		Body:        body,
+	}, nil
+}
+type HTTPResponse struct {
+	StatusCode int
+	Headers    map[string]string
+	Body       []byte
+}
+func (r HTTPResponse) String() string {
+	response := fmt.Sprintf("HTTP/1.1 %d %s\r\n", r.StatusCode, http.StatusText(r.StatusCode))
+	for k, v := range r.Headers {
+		response += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	response += "\r\n" + string(r.Body)
+	return response
+}
+func (r HTTPResponse) Write(connection net.Conn) error {
+	_, err := connection.Write([]byte(r.String()))
+	return err
+}
+func textResponse(statusCode int, body string) HTTPResponse {
+	contentLength := len(body)
+	return HTTPResponse{
+		StatusCode: statusCode,
+		Headers:    map[string]string{"Content-Type": "text/plain", "Content-Length": fmt.Sprintf("%d", contentLength)},
+		Body:       []byte(body),
+	}
+}
+func fileResponse(statusCode int, file *os.File) HTTPResponse {
+	stat, err := file.Stat()
+	if err != nil {
+		return textResponse(500, err.Error())
+	}
+	contentLength := stat.Size()
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		return textResponse(500, err.Error())
+	}
+	return HTTPResponse{
+		StatusCode: statusCode,
+		Headers:    map[string]string{"Content-Type": "application/octet-stream", "Content-Length": fmt.Sprintf("%d", contentLength)},
+		Body:       contents,
+	}
+}
+type HTTPHandler func(HTTPRequest) HTTPResponse
+type HTTPRouter struct {
+	routes map[string]HTTPHandler
+}
+func NewHTTPRouter() HTTPRouter {
+	return HTTPRouter{
+		routes: make(map[string]HTTPHandler),
+	}
+}
+func (s *HTTPRouter) AddEndpoint(method string, prefix string, handler HTTPHandler) {
+	s.routes[method+prefix] = handler
+}
+func (s *HTTPRouter) GetHandler(method string, prefix string) HTTPHandler {
+	if handler, ok := s.routes[method+prefix]; ok {
+		return handler
+	}
+	for routeInfo := range s.routes {
+		if strings.HasSuffix(routeInfo, "*") && strings.HasPrefix(method+prefix, routeInfo[:len(routeInfo)-1]) {
+			return s.routes[routeInfo]
+		}
+	}
+	return nil
+}
+func (s HTTPRouter) handleConnection(connection net.Conn) error {
+	defer connection.Close()
+	request, err := parseRequest(connection)
+	if err != nil {
+		fmt.Println("Failed to parse request: ", err.Error())
+		return err
+	}
+	handler := s.GetHandler(request.Method, request.Target)
+	var response HTTPResponse
+	if handler == nil {
+		response = HTTPResponse{StatusCode: 404}
+	} else {
+		response = handler(request)
+	}
+	if err := response.Write(connection); err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+		return err
+	}
+	fmt.Println("Processed request: ", request.String())
+	return nil
+}
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	var directory string
+	flag.StringVar(&directory, "directory", "", "Directory to serve")
+	flag.Parse()
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	router := NewHTTPRouter()
+	router.AddEndpoint("GET", "/", func(request HTTPRequest) HTTPResponse {
+		return HTTPResponse{StatusCode: 200}
+	})
+	router.AddEndpoint("GET", "/echo/*", func(request HTTPRequest) HTTPResponse {
+		data := strings.TrimPrefix(request.Target, "/echo/")
+		response := textResponse(200, data)
+		encoding, ok := request.Headers["Accept-Encoding"]
+		if ok && encoding == "gzip" {
+			response.Headers["Content-Encoding"] = encoding
+		}
+		return response
+	})
+	router.AddEndpoint("GET", "/user-agent", func(request HTTPRequest) HTTPResponse {
+		value, ok := request.Headers["User-Agent"]
+		if !ok {
+			return HTTPResponse{StatusCode: 400}
+		}
+		return textResponse(200, value)
+	})
+	router.AddEndpoint("GET", "/files/*", func(request HTTPRequest) HTTPResponse {
+		filePath := strings.TrimPrefix(request.Target, "/files/")
+		file, err := os.Open(filepath.Join(directory, filePath))
+		if err != nil {
+			return HTTPResponse{StatusCode: 404}
+		}
+		defer file.Close()
+		return fileResponse(200, file)
+	})
+	router.AddEndpoint("POST", "/files/*", func(request HTTPRequest) HTTPResponse {
+		filePath := strings.TrimPrefix(request.Target, "/files/")
+		file, err := os.Create(filepath.Join(directory, filePath))
+		if err != nil {
+			return HTTPResponse{StatusCode: 500}
+		}
+		defer file.Close()
+		_, err = file.Write(request.Body)
+		if err != nil {
+			return HTTPResponse{StatusCode: 500}
+		}
+		return HTTPResponse{StatusCode: 201}
+	})
 	for {
-		conn, err := l.Accept()
+		connection, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
 		}
-		go handleConn(conn)
+		fmt.Println("Connected: ", connection.RemoteAddr())
+		go router.handleConnection(connection)
 	}
-}
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error handling request: ", err.Error())
-		os.Exit(1)
-	}
-	r := strings.Split(string(buf), "\r\n")
-	m := strings.Split(r[0], " ")[0]
-	p := strings.Split(r[0], " ")[1]
-	var response string
-	if m == "GET" && p == "/" {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if m == "GET" && p[0:6] == "/echo/" {
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(p[6:]), p[6:])
-	} else if m == "GET" && p == "/user-agent" {
-		ua := strings.Split(r[2], " ")[1]
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(ua), ua)
-	} else if m == "GET" && p[0:7] == "/files/" {
-		dir := os.Args[2]
-		content, err := os.ReadFile(path.Join(dir, p[7:]))
-		if err != nil {
-			response = "HTTP/1.1 404 Not Found\r\n\r\n"
-		} else {
-			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), string(content))
-		}
-	} else if m == "POST" && p[0:7] == "/files/" {
-		content := strings.Trim(r[len(r)-1], "\x00")
-		dir := os.Args[2]
-		_ = os.WriteFile(path.Join(dir, p[7:]), []byte(content), 0644)
-		response = "HTTP/1.1 201 Created\r\n\r\n"
-	} else {
-		response = "HTTP/1.1 404 Not Found\r\n\r\n"
-	}
-	conn.Write([]byte(response))
 }
