4e3753aacbc01873ea47b354a3e6bd061551756d
dheerajcl <cldheeraj541@gmail.com>
2024-06-05 10:00:24 +0530

    Including User Agent

--- app/server.go
+++ app/server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -19,20 +20,27 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	
-	buffer:= make([]byte, 1024)
 
-	conn.Read(buffer)
+	defer conn.Close()
 
-	splitHeader:=strings.Split(string(buffer), "\r\n")
-	splitRequestLine:=strings.Split(splitHeader[0], " ")
-
-	if splitRequestLine[1] == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.Split(splitRequestLine[1], "/")[1] == "echo" {
-		requestBody:=strings.Split(splitRequestLine[1], "/")[2]
-		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+fmt.Sprint(len(requestBody))+"\r\n\r\n"+requestBody))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		fmt.Println("Error reading request. ", err.Error())
+		return
 	}
+	
+	var res string
+	path:=request.URL.Path
+
+	if path == "/" {
+		res = "HTTP/1.1 200 OK\r\n\r\n"
+	} else if path == "/user-agent" {
+		res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(request.UserAgent()), request.UserAgent())
+	} else if path[0:6] == "/echo/" {
+		echo := path[6:]
+		res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo), echo)
+	} else {
+		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	conn.Write([]byte(res))
 }
