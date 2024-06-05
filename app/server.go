138a4ecd1425bf70e25f06200efc252d8dfac5d6
dheerajcl <cldheeraj541@gmail.com>
2024-06-05 10:23:40 +0530

    Post a file

--- app/server.go
+++ app/server.go
@@ -1,74 +1,62 @@
 package main
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
-	"strconv"
+	"path"
 	"strings"
 )
-type Request struct {
-	verb       string
-	path       string
-	version    string
-	host       string
-	user_agent string
-}
-func parseReq(req string) Request {
-	var Req Request
-	req_lines := strings.Split(req, "\r\n")
-	first_line := strings.Split(req_lines[0], " ")
-	Req.verb = first_line[0]
-	Req.path = first_line[1]
-	Req.version = first_line[2]
-	Req.host = strings.Split(req_lines[1], ":")[1]
-	Req.user_agent = strings.Split(req_lines[2], ":")[1]
-	return Req
-}
-func handleConnection(c net.Conn) {
-	buffer := make([]byte, 5000)
-	c.Read(buffer)
-	req := string(buffer)
-	// Req := parseReq(req)
-	// fmt.Println(Req)
-	first_line := strings.Split(strings.Split(req, "\r\n")[0], " ")
-	if strings.HasPrefix(first_line[1], "/echo/") {
-		str := strings.Split(first_line[1], "/")[2]
-		c.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(str), str)))
-	} else if strings.HasPrefix(first_line[1], "/files/") {
-		directory := os.Args[2]
-		str := strings.Split(first_line[1], "/")[2]
-		data, err := os.ReadFile(directory + str)
-		if err != nil {
-			c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		} else {
-			c.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + strconv.Itoa(len(data)) + "\r\n\r\n" + string(data) + "\r\n\r\n"))
-		}
-	} else if first_line[1] == "/" {
-		c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	}
-	if first_line[1] == "/user-agent" {
-		// req_agent := strings.Trim(Req.user_agent, " ")
-		user_agent := strings.Trim(strings.Split(strings.Split(req, "\r\n")[2], ":")[1], " ")
-		c.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(user_agent), user_agent)))
-	} else {
-		c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	}
-}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	log.Println("Server running at localhost:4221")
-	l, err := net.Listen("tcp", ":4221")
+	fmt.Println("Logs from your program will appear here!")
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		log.Printf("Failed to bind to port 4221")
-		os.Exit(69)
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
 	}
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Print("Unable to handle connection")
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+		go handleConn(conn)
 	}
+}
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error handling request: ", err.Error())
+		os.Exit(1)
+	}
+	r := strings.Split(string(buf), "\r\n")
+	m := strings.Split(r[0], " ")[0]
+	p := strings.Split(r[0], " ")[1]
+	var response string
+	if m == "GET" && p == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else if m == "GET" && p[0:6] == "/echo/" {
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(p[6:]), p[6:])
+	} else if m == "GET" && p == "/user-agent" {
+		ua := strings.Split(r[2], " ")[1]
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%d\r\n\r\n%s", len(ua), ua)
+	} else if m == "GET" && p[0:7] == "/files/" {
+		dir := os.Args[2]
+		content, err := os.ReadFile(path.Join(dir, p[7:]))
+		if err != nil {
+			response = "HTTP/1.1 404 Not Found\r\n\r\n"
+		} else {
+			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), string(content))
+		}
+	} else if m == "POST" && p[0:7] == "/files/" {
+		content := strings.Trim(r[len(r)-1], "\x00")
+		dir := os.Args[2]
+		_ = os.WriteFile(path.Join(dir, p[7:]), []byte(content), 0644)
+		response = "HTTP/1.1 201 Created\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	conn.Write([]byte(response))
 }
